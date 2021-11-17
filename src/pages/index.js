6a6a4b84a58ecc3f0a923d31e08fa74c39833cc4
Flaturtha <richthewriterguy@gmail.com>
2021-11-17 12:25:13 -0600

    hp clean up

--- src/pages/index.js
+++ src/pages/index.js
@@ -18,9 +18,12 @@ export default function Home() {
           Non-Techie&nbsp;Guides!
         </h1>
         <h2 className="mx-auto my-4 text-center text-xl font-bold text-gray-600">Git yur nErd on!</h2>
-        <p className="mx-auto mb-8 text-center text-gray-400 italic">the non-techies guides to all things tech &hellip; from one non-techie to another!</p>
-        <img className="mx-auto max-w-full" src="https://timer.getmara.com/c3R5bGU9MTImZGF0ZT0yMDIxLTEyLTAxJnRpbWU9MDklM0EwMCZ0aW1lem9uZT1BbWVyaWNhJTJGQ2hpY2FnbyZiY2tDb2xvcj1GRkZGRkYmZm9udENvbG9yPTAwMDAwMCZsYWJlbENvbG9yPTQ0NDQ0NCZkYXlMYWJlbD1kYXlzJmhvdXJMYWJlbD1ob3VycyZtaW5MYWJlbD1taW4mc2VjTGFiZWw9c2Vj.gif" />
-        
+        <p className="prose mx-auto mb-8 text-center text-gray-400 italic">the non-techies guides to all things tech &hellip; from one non-techie to another!</p>
+        <p className="prose mx-auto mb-8 text-center text-gray-600">Guides, blogs, tutorials, &amp; a complete a-to-z guide book to help <em>you</em> build a faster, more secure, and Google-compliant website &hellip; in less time and lower cost!</p>
+        <div className="border-gray-400 border-2 border-solid p-4 mx-auto text-center prose">
+          <h2>LAUNCHING IN &hellip;</h2>
+          <img className="mx-auto max-w-full" src="https://timer.getmara.com/c3R5bGU9MTImZGF0ZT0yMDIxLTEyLTAxJnRpbWU9MDklM0EwMCZ0aW1lem9uZT1BbWVyaWNhJTJGQ2hpY2FnbyZiY2tDb2xvcj1GRkZGRkYmZm9udENvbG9yPTAwMDAwMCZsYWJlbENvbG9yPTQ0NDQ0NCZkYXlMYWJlbD1kYXlzJmhvdXJMYWJlbD1ob3VycyZtaW5MYWJlbD1taW4mc2VjTGFiZWw9c2Vj.gif" />
+        </div>
       </main>
 
     </div>

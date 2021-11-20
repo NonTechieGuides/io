0fc435a21cfef036ec77a8683288df3f29df1680
Flaturtha <richthewriterguy@gmail.com>
2021-11-20 11:15:02 -0600

    adds new logo

--- src/components/nav.js
+++ src/components/nav.js
@@ -1,18 +1,20 @@
 const Nav = () => {
   return (
-    <nav className="mx-auto mt-4 mb-8 md:mb-16">
+    <nav className=" mx-auto mb-8 md:mb-16 pb-2 border-b-2 border-magenta">
         <img
-          className="mx-auto mb-2 w-12 sm:w-24"
+          className="mx-auto w-14"
           alt="Non-Techie Guides"
-          src="/assets/img_logo_white.svg"
+          src="/assets/color-code-logo_simple.png"
         />
         <h1 className="font-mono mx-auto text-center uppercase text-3xl md:text-4xl font-bold">
-          Non-Techie&nbsp;Guides!
+          Non-Techie&nbsp;Guides
         </h1>
-        <p className="mx-auto mb-8 text-center text-magenta italic">git yur nErd on!</p>
+        <p className="mx-auto text-center text-cyan italic">git yur nErd on!</p>
 
       </nav>
   )
 }
 
 export default Nav
+
+{/!/}
